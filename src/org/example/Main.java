dd4c3f52542e15bd97051e4a586b4984a2cf68bf
marishine <tkachenko.m.r@gmail.com>
2026-03-09 14:14:36 +0300

    Обновление кода

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         if(args.length!= 1){
+
             System.out.println("Usage: java Main <filename>");
             return;
         }
